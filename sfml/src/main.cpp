38c1f64998312d48a8976c898f8418454b41d08f
marcelochincha <103434714+marcelochincha@users.noreply.github.com>
2023-11-17 07:30:43 -0500

    graphics template

---
+++ sfml/src/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow{ { 640, 480 }, "CMake SFML Project" };
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        for (auto event = sf::Event{}; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+}
